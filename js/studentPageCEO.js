6cedbf884c12d61f50bdbb535de948104dff39d9
ChadoNihi <chadoishere@gmail.com>
2016-11-27 23:01:58 +0200

    save

---
+++ js/studentPageCEO.js
@@ -0,0 +1,12 @@
+const projects = [
+  {},
+  {}.
+  {}
+];
+
+//on ready
+document.addEventListener('DOMContentLoaded', function() {
+  var projectsTbody = document.getElementById('ceo-student-projects-tbody');
+
+  projects.forEach();
+}, false);
